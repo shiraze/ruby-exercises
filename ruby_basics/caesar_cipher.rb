c54a7a8f280110e02cdb59460137f69507ae49c2
Shiraz Esat <shiraz.esat@madetech.com>
2022-09-12 13:26:07 +0100

    Fix formatting issues
    Using "rubocop -x caesar_cipher.rb"
    Note that this fixes some linting issues, but not all

--- ruby_basics/caesar_cipher.rb
+++ ruby_basics/caesar_cipher.rb
@@ -1,24 +1,22 @@
 def caesar_cipher(string, shift)
-    shift %= 26
-    string.codepoints.map do |char|
-             
-          #65 - 90 A-Z
-          #97 - 122 a-z
-          if char.between?(65,90) 
-            new_char = char + shift
-            new_char-=26 if new_char > 90
-            new_char+=26 if new_char < 65
-            new_char.chr 
-         elsif char.between?(97,122)
-            new_char = char + shift
-            new_char-=26 if new_char > 122
-            new_char+=26 if new_char < 97
-            new_char.chr 
-          else
-            char.chr
-          end
+  shift %= 26
+  string.codepoints.map do |char|
+    # 65 - 90 A-Z
+    # 97 - 122 a-z
+    if char.between?(65, 90)
+      new_char = char + shift
+      new_char -= 26 if new_char > 90
+      new_char += 26 if new_char < 65
+      new_char.chr
+    elsif char.between?(97, 122)
+      new_char = char + shift
+      new_char -= 26 if new_char > 122
+      new_char += 26 if new_char < 97
+      new_char.chr
+    else
+      char.chr
+    end
+  end.join
+end
 
-       end.join
-  end
-  
-  puts caesar_cipher("What a string!", 1000)
+puts caesar_cipher("What a string!", 1000)
